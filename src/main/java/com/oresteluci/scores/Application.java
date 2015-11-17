845e1fc1303bf8b08f5ff30e46053ee08a6d394d
Oreste Luci <oreste@luci.cl>
2015-11-18 00:10:02 +0100

    Refactoring

--- src/main/java/com/oresteluci/scores/Application.java
+++ src/main/java/com/oresteluci/scores/Application.java
@@ -18,5 +18,4 @@ public class Application {
     public static void main(String[] args) throws Exception {
         ServerInitialization.run("com.oresteluci.scores", "com.oresteluci.scores.server.ServerImpl", args);
     }
-    
 }
